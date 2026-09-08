093c689cb938368193ebabb5193e1f6841dd68b8
likhithaj04 <likhitha15jayram@gmail.com>
2026-09-08 22:14:22 +0530

    added render url

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import userMailRouter from './routes/userMailRoute.js'
 import { apiLimiter } from './Middleware/rateLimitter.js'
 
 // app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
-app.use(cors({ origin: 'https://inboxly-steel.vercel.app/', credentials: true }))
+app.use(cors({ origin: 'https://inboxly-steel.vercel.app', credentials: true }))
 
 app.use(express.json())
 app.use(cookieParser())
